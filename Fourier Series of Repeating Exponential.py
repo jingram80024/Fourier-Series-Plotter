e30354333707c4893f422f93c40176ea807c3a42
jingram80024 <102332226+jingram80024@users.noreply.github.com>
2022-05-07 18:52:17 -0400

    Push Initial Revision
    First working copy of fourier series of repeating exponential plotter. Program will run for more than 10 minutes if 100k composite sinusoids are used.

---
+++ Fourier Series of Repeating Exponential.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+a_0 = ((math.exp(2*math.pi)-1)/(math.pi))
+a_n = []
+b_n = []
+resolution = 10000
+
+for n in range(resolution):
+    a_x = ((math.exp(2*math.pi))-1)/((math.pi*((n+1)**2))+math.pi)
+    a_n.append(a_x)
+    b_x = ((n+1)-((n+1)*math.exp(2*math.pi)))/((math.pi*((n+1)**2))+math.pi)
+    b_n.append(b_x)
+
+w_t = np.arange(0,(4*math.pi),(math.pi/10000));
+
+fig, ax = plt.subplots(figsize=(12,5))
+ax.set_xlabel('wt')
+ax.set_ylabel('f(wt)')
+ax.set_title("Fourier Series of exp^(wt)")
+n = 0
+f_x = a_0/2
+for n in range(resolution):
+    f_x = f_x + a_n[n]*np.cos((n+1)*w_t)
+    f_x = f_x + b_n[n]*np.sin((n+1)*w_t)
+    if (n == 0 or n == 3 or n == 6 or n == 9999):
+        ax.plot(w_t, f_x, label=(n+1))
+        
+ax.legend();
+        
