391d43a1d069f89e46a980cbe5ecfa202bc8f746
jingram80024 <102332226+jingram80024@users.noreply.github.com>
2022-08-01 22:50:14 -0400

    Provide Sample Plot

--- Fourier Series of Repeating Exponential.py
+++ Fourier Series of Repeating Exponential.py
@@ -5,7 +5,7 @@ import math
 a_0 = ((math.exp(2*math.pi)-1)/(math.pi))
 a_n = []
 b_n = []
-resolution = 10000
+resolution = 100000
 
 for n in range(resolution):
     a_x = ((math.exp(2*math.pi))-1)/((math.pi*((n+1)**2))+math.pi)
@@ -15,16 +15,18 @@ for n in range(resolution):
 
 w_t = np.arange(0,(4*math.pi),(math.pi/10000));
 
+plt.style.use('dark_background')
 fig, ax = plt.subplots(figsize=(12,5))
 ax.set_xlabel('wt')
 ax.set_ylabel('f(wt)')
 ax.set_title("Fourier Series of exp^(wt)")
+
 n = 0
 f_x = a_0/2
 for n in range(resolution):
     f_x = f_x + a_n[n]*np.cos((n+1)*w_t)
     f_x = f_x + b_n[n]*np.sin((n+1)*w_t)
-    if (n == 0 or n == 3 or n == 6 or n == 9999):
+    if (n == 0 or n == 3 or n == 6 or n == 99999):
         ax.plot(w_t, f_x, label=(n+1))
         
 ax.legend();
